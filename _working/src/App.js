07c5cecfc26c03306c8bc1ffdfe13f31ec38c134
shohan <shohan.ece17@gmail.com>
2023-11-27 18:46:57 +0600

    holaa

--- _working/src/App.js
+++ _working/src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { useEffect, useState } from 'react';
+import Countries from './Components/Countries/Countries';
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
+      <Countries></Countries>
     </div>
   );
 }
--- react_to-do-list-app/src/App.js
+++ react_to-do-list-app/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { useEffect, useState } from 'react';
 import { AiOutlineDelete } from 'react-icons/ai';
