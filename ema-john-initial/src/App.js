b1d690d1c62aa80f526b0e52d5bceca0bfa47be2
shohan <shohan.ece17@gmail.com>
2023-11-30 18:39:29 +0600

    next phase

--- ema-john-initial/src/App.js
+++ ema-john-initial/src/App.js
@@ -1,12 +1,44 @@
 import './App.css';
-import Header from './components/Header/Header';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './layout/Main';
+import About from './components/About/About';
 import Shop from './components/Shop/Shop';
+import Orders from './components/Orders/Orders';
+import Inventory from './components/Inventory/Inventory';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Shop></Shop>
+        },
+        {
+          path: 'shop',
+          element: <Shop></Shop>
+        },
+        {
+          path: 'orders',
+          element: <Orders></Orders>
+        },
+        {
+          path: 'inventory',
+          element: <Inventory></Inventory>
+        },
+        {
+          path: 'about',
+          element: <About></About>
+        }
+      ]
+    }
+
+  ])
   return (
     <div>
-      <Header></Header>
-      <Shop></Shop>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ ema-john-initial/src/components/Inventory/Inventory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Inventory = () => {
+   return (
+      <div>
+         <p>what?</p>
+      </div>
+   );
+};
+
+export default Inventory;
