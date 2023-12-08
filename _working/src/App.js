39e1c8161be616c4a819687358dc3e6a6b93b924
shohan <shohan.ece17@gmail.com>
2023-12-09 02:26:17 +0600

    poor performance

--- _working/src/App.js
+++ _working/src/App.js
@@ -1,18 +1,44 @@
 import './App.css';
-// import React, { useEffect, useState } from 'react';
-import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
+import React, { useEffect, useState } from 'react';
+import { GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithPopup, signOut } from 'firebase/auth';
 import app from './firebase/firebase.init';
 
 
 const auth = getAuth(app);
 
 function App() {
+  const [user, setUser] = useState({});
   const provider = new GoogleAuthProvider();
+  const gitProvider = new GithubAuthProvider();
 
   const handleGoogleSignIn = () => {
     signInWithPopup(auth, provider)
       .then((result) => {
         const user = result.user;
+        setUser(user);
+        // console.log(user);
+      })
+      .catch(error => {
+        console.log('Error: ', error);
+      })
+  }
+  const handleGoogleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({});
+        console.log('Sign-out successful');
+      })
+      .catch(error => {
+        setUser({});
+        console.log('Error: ', error);
+      })
+  }
+
+  const handleGitHubSignIn = () => {
+    signInWithPopup(auth, gitProvider)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
         console.log(user);
       })
       .catch(error => {
@@ -22,7 +48,22 @@ function App() {
 
   return (
     <div className="App">
-      <button onClick={handleGoogleSignIn}><h3>Google sign in</h3></button>
+      {
+        user.uid ?
+          <button onClick={handleGoogleSignOut}><h3>sign out</h3></button>
+          :
+          <>
+            <button onClick={handleGoogleSignIn}><h3>Google sign in</h3></button>
+            <button onClick={handleGitHubSignIn}><h3>GitHub sign in</h3></button>
+          </>
+      }
+      {
+
+        user.uid && <div>
+          <h3>User name : {user.displayName}</h3>
+          <p>Email Address : {user.email}</p>
+        </div>
+      }
     </div>
   );
 }
