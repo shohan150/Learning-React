6d125baca013067880ae9e2d5836671b00f45e9d
shohan <shohan.ece17@gmail.com>
2023-12-22 02:17:29 +0600

    repo inside repo!!!!

--- _working/src/pages/shared/leftnav/LeftNav.js
+++ _working/src/pages/shared/leftnav/LeftNav.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftNav = () => {
+
+   const [categories, setCategories] = useState([]);
+   useEffect(() => {
+      fetch('http://localhost:5000/categories')
+         .then(res => res.json())
+         .then(data => setCategories(data))
+         .catch(err => console.log(err))
+   }, []);
+
    return (
       <div>
-         this is left nav
+         <h4>All Categories</h4>
+         <div className='ps-4'>
+            {
+               categories.map(cat => <p key={cat.id}>
+                  <Link to={`/category/${cat.id}`} className='text-decoration-none text-black'>{cat.name}</Link>
+               </p>)
+            }
+         </div>
       </div>
    );
 };
