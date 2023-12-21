0e169f0680f18d9274731d2fa55a34c2f4fa2d55
shohan <shohan.ece17@gmail.com>
2023-12-21 20:16:16 +0600

    62.6 half

--- _working/src/pages/shared/rightnav/RightNav.js
+++ _working/src/pages/shared/rightnav/RightNav.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Button, ListGroup } from 'react-bootstrap';
+import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
 
 const RightNav = () => {
    return (
       <div>
-         this is right nav
+         <h4>Login with</h4>
+         <Button mb-2 variant='outline-primary'><FaGoogle />Login with Google</Button>
+         <Button variant='outline-secondary'><FaGithub />Login with Github</Button>
+
+         <div>
+            Find us on
+            <ListGroup>
+               <ListGroup.Item><FaFacebook /> Facebook</ListGroup.Item>
+               <ListGroup.Item><FaTwitter /> Twitter</ListGroup.Item>
+               <ListGroup.Item><FaInstagram /> Instagram</ListGroup.Item>
+            </ListGroup>
+         </div>
+
       </div>
+
    );
 };
 
