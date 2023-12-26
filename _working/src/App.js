1bbc90d5bc94828cc8ea210f5878ea5a9c87d778
shohan <shohan.ece17@gmail.com>
2023-12-26 18:02:18 +0600

    start new app

---
+++ _working/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+// import React, { useEffect, useState } from 'react';
+
+
+function App() {
+  return (
+    <>
+
+    </>
+  );
+}
+
+export default App;
--- ph-projects/dragon-news/src/index.js
+++ ph-projects/dragon-news/src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './routes/routes';
+import AuthProvider from './providers/AuthProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <RouterProvider router={router}></RouterProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
 
